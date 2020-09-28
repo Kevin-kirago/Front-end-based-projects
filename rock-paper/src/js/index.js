94a1846a1ebb6c7ebe4640a6a4a903cff135c7ba
Kevin <tkevinkiragu@gmail.com>
2020-09-29 02:14:57 +0300

    updating npm packages

--- rock-paper/src/js/index.js
+++ rock-paper/src/js/index.js
@@ -1,1 +1,16 @@
-// create a data structure to store our data
+window.state = {
+	isPlaying: true,
+};
+
+// varibale
+let playBoard, scoreBoard, rock, paper, scisssors;
+playBoard = document.querySelector(".content__playboard");
+scoreBoard = document.querySelector(".content__scoreboard");
+
+if (state.isPlaying) {
+	playBoard.display = "grid";
+	scoreBoard.display = "none";
+} else {
+	playBoard.display = "none";
+	scoreBoard.display = "flex";
+}
