b59f950cb30d45510801a6006a6448165205272a
Kevin <tkevinkiragu@gmail.com>
2021-01-26 12:56:56 +0300

    project package update

--- rest-countries-api-with-color-theme-switcher-master/src/components/navigation.component.jsx
+++ rest-countries-api-with-color-theme-switcher-master/src/components/navigation.component.jsx
@@ -4,6 +4,11 @@ const Navigation = () => {
 	return (
 		<div className="navigation">
 			<span className="navigation__title">Where in the world?</span>
+			<div className="navigation__list">
+				<span className="navigation__icon">
+					<i className="ion-ios-moon-outline"></i>
+				</span>
+			</div>
 		</div>
 	);
 };
