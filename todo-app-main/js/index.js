7a3d2f0c96db1ac5c7b7b02baebcc2de23d84a54
Kevin <tkevinkiragu@gmail.com>
2021-03-02 22:14:07 +0300

    added an event listener for functional testing

---
+++ todo-app-main/js/index.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+const headerBackground = document.querySelector(".header");
+const darkModeBtn = document.querySelector(".mode__img");
+
+darkModeBtn.addEventListener("click", (e) => {
+	console.log(e.target.src);
+});
